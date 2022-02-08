1777ef71f4fc309168921a36677459ab957fd6db
mrhrifat <mrhrifat383759@gmail.com>
2022-02-08 13:59:09 +0600

    refactor: Routs & Route of different Components

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Route, Routes } from 'react-router-dom';
 import Dashboard from './components/Header/Dashboard';
 import Hero from './components/Hero';
 import SurahList from './components/SurahList';
@@ -8,10 +9,16 @@ class Root extends Component {
     render() {
         return (
             <>
-                <Dashboard />
-                <Hero />
-                <SurahList />
-                <Verses />
+                <Routes>
+                    <Route path='/' element={<Dashboard />} />
+                </Routes>
+                <Routes>
+                    <Route path='/' element={<Hero />} />
+                </Routes>
+                <Routes>
+                    <Route path='/' element={<SurahList />} />
+                    <Route path="/:chapterId" element={<Verses />} />
+                </Routes>
             </>
         )
     }
