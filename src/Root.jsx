ccc8b3df50c33b7b8aeffee842a15b64ecd49052
mrhrifat <mrhrifat383759@gmail.com>
2022-02-09 11:45:06 +0600

    feat: add Rooter component Route

--- src/Root.jsx
+++ src/Root.jsx
@@ -5,7 +5,7 @@ import Hero from './components/Hero';
 import SurahInfo from './components/SurahInfo';
 import SurahList from './components/SurahList';
 import Tafsir from './components/Tafsir';
-import { Verses } from './components/Verses';
+import Footer from './components/Footer';
 
 class Root extends Component {
     render() {
@@ -22,6 +22,9 @@ class Root extends Component {
                     <Route path="/surah/:chapterId" element={<Verses />} />
                     <Route path="/surah/:chapterId/info" element={<SurahInfo />} />
                     <Route path='/surah/:chapterId/tafsir' element={<Tafsir />} />
+                </Routes>
+                <Routes>
+                    <Route path='/' element={<Footer />} />
                 </Routes>
             </>
         )
