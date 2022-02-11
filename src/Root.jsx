b4a65ebc5c3437fbed7306c8981e5e8e971d28b2
mrhrifat <mrhrifat383759@gmail.com>
2022-02-12 01:19:45 +0600

    del: remove route for upgrade

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Outlet, Route, Routes } from 'react-router-dom';
 import Dashboard from './components/Header/Dashboard';
 import Hero from './components/Hero';
 import SurahInfo from './components/SurahInfo';
 import SurahList from './components/SurahList';
-import Tafsir from './components/Tafsir';
 import Verses from './components/Verses';
 import Footer from './components/Footer';
 import NotFound from './components/Others/NotFound';
@@ -13,24 +12,9 @@ class Root extends Component {
     render() {
         return (
             <>
-                <Routes>
-                    <Route path='/' element={<Dashboard />} />
-                </Routes>
-                <Routes>
-                    <Route path='/' element={<Hero />} />
-                </Routes>
-                <Routes>
-                    <Route path='*' element={<NotFound />} />
-                    <Route path='/' element={<SurahList />} />
-                    <Route path="/surah/:chapterId" element={<Verses />} />
-                    <Route path="/surah/:chapterId/info" element={<SurahInfo />} />
-                    <Route path='/surah/:chapterId/tafsir' element={<Tafsir />} />
-                </Routes>
-                <Routes>
-                    <Route path='/' element={<Footer />} />
-                </Routes>
+                
             </>
-        )
+        );
     }
 }
 
