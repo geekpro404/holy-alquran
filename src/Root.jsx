5e27ad68437a66b16dca14f9288b121ad1727f73
mrhrifat <mrhrifat383759@gmail.com>
2022-02-13 14:14:14 +0600

    feat: route of 3 components

--- src/Root.jsx
+++ src/Root.jsx
@@ -8,6 +8,11 @@ import Verses from './components/Verses';
 import Footer from './components/Footer';
 import NotFound from './components/Others/NotFound';
 import AboutUs from './components/Others/AboutUs';
+import Feedback from './components/Others/Feedback';
+import BugReports from './components/Others/BugReports';
+import Contribution from './components/Others/Contribution';
+import Privacy from './components/Others/Privacy';
+
 
 class Root extends Component {
     render() {
@@ -31,7 +36,10 @@ class Root extends Component {
                     <Route path='/surah/:chapterId/info' element={<SurahInfo />} />
                     <Route path='*' element={<NotFound />} />
                     <Route path='/about-us' element={<AboutUs />} />
-                    <Route path='/feedback' element={<AboutUs />} />
+                    <Route path='/feedback' element={<Feedback />} />
+                    <Route path='/contribution' element={<Contribution />} />
+                    <Route path='/bug-reports' element={<BugReports />} />
+                    <Route path='/privacy' element={<Privacy />} />
                 </Routes>
             </>
         );
