b44dbdbb60e9d1eda71e3751d797d7b70260f053
mrhrifat <mrhrifat383759@gmail.com>
2022-02-17 22:53:19 +0600

    feat: create footer component with fragment

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+import classes from '../styles/all.module.css'
+
+const Footer = () => {
+    return (
+        <>
+            
+        </>
+    )
+};
+
+export default Footer;
