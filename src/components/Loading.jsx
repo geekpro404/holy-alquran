5c19b0038723b93c25357d41353b55e49a57d5cb
mrhrifat <mrhrifat383759@gmail.com>
2022-02-17 23:01:52 +0600

    styles: change spinner color style

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Spinner } from 'react-bootstrap'
+import classes from '../styles/all.module.css'
 
 const Loading = () => {
     return (
         <div className="container">
             <div className="row">
-                <div className="col-md-12 text-center">
-                    <Spinner animation="border" />
+                <div className="col-md-12 py-5 text-center">
+                    <Spinner animation="border" className={classes.spinner}/>
                 </div>
             </div>
         </div>
