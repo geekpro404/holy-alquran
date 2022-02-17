5ff89a20ce6ae432acce832e85cb555ba52cc766
mrhrifat <mrhrifat383759@gmail.com>
2022-02-17 22:43:17 +0600

    refactor: create navbar with offcanvas layout

--- src/components/Header/Home.jsx
+++ src/components/Header/Home.jsx
@@ -11,7 +11,20 @@ import classes from '../../styles/all.module.css';
 
 const Home = () => {
     return (
-        
+        <Navbar collapseOnSelect bg="light" expand={false}>
+            <Navbar.Toggle aria-controls="offcanvasNavbar" className={classes.homeIcon} />
+            <Navbar.Offcanvas
+                id="offcanvasNavbar"
+                aria-labelledby="offcanvasNavbarLabel"
+                placement="end"
+            >
+
+                <Offcanvas.Header closeButton>
+                    <Offcanvas.Title id="offcanvasNavbarLabel" className={classes.brandLink}>Al Quran</Offcanvas.Title>
+                </Offcanvas.Header>
+                
+            </Navbar.Offcanvas>
+        </Navbar>
     )
 };
 
