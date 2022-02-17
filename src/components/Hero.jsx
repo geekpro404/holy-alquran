f2c5f47b8c403966ec0f2ae2e12d39777910873d
mrhrifat <mrhrifat383759@gmail.com>
2022-02-17 22:58:06 +0600

    refactor: remove search section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import classes from '../styles/all.module.css';
+import Search from './Search';
+
+
+const Hero = () => {
+
+    return (
+        
+    )
+};
+
+export default Hero;
