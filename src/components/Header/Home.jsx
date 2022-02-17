14ef327e5cc2a88cfbc78cf3ab2f5c6c8781d0d0
mrhrifat <mrhrifat383759@gmail.com>
2022-02-17 22:41:56 +0600

    feat: create funcational home component

---
+++ src/components/Header/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Navbar, Offcanvas, Nav } from 'react-bootstrap'
+import { Link } from 'react-router-dom';
+import { BsInfoCircle } from 'react-icons/bs'
+import { AiOutlineHome } from 'react-icons/ai'
+import { MdOutlineFeedback, MdOutlinePrivacyTip } from 'react-icons/md'
+import { FaRegEdit } from 'react-icons/fa'
+import classes from '../../styles/all.module.css';
+
+
+
+const Home = () => {
+    return (
+        
+    )
+};
+
+export default Home
